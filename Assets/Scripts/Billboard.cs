0f4ae6f0fd9fbb9e49ccf8aa27384a3924efe722
ekremtahaunlu <ekremtahaunlu@gmail.com>
2025-09-11 11:08:53 +0300

    Bilboard edited and warrior asset added

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -1,26 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteAlways]
 public class Billboard : MonoBehaviour
 {
-    Camera cam;
-
-    void Start()
-    {
-        cam = Camera.main;
-    }
+    private Camera cam;
 
     void LateUpdate()
     {
-        if (cam == null) cam = Camera.main;
-        if (cam == null) return;
+        if (cam == null)
+            cam = Camera.main;
 
-        // Y eksenini sabitleyerek döndür (upright billboard)
+        if (cam == null)
+            return;
+
         Vector3 dir = cam.transform.position - transform.position;
-        dir.y = 0f; // eğer tam 3D dönüş istersen bu satırı kaldır
+
+        dir.y = 0f;
+
         if (dir.sqrMagnitude > 0.001f)
+        {
             transform.rotation = Quaternion.LookRotation(dir);
+        }
     }
 }
